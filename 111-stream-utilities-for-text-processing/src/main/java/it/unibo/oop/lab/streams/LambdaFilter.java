6bc0795282abd2cb6c1f25831fcd6689b2b69720
Enrico Solaroli <enrico.solaroli@studio,unibo,it>
2025-12-02 15:15:36 +0100

    correct es  111

--- 111-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
+++ 111-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
@@ -45,18 +45,22 @@ public final class LambdaFilter extends JFrame {
          */
         IDENTITY("No modifications", Function.identity()),
         TO_LOWER("convert to lowercase", String::toLowerCase),
-        COUNT_CHAR("Count the number of chars", s -> String.valueOf(s.chars().count())),
+        COUNT_CHAR("Count the number of chars", s -> String.valueOf(s.length())),
         COUNT_LINES("Count the number of lines", s -> String.valueOf(s.lines().count())),
-        ALPHABETICAL_ORDER("List all the words in alphabetical order", 
-            s -> Arrays.stream(s.split(" "))
-            .sorted(String::compareToIgnoreCase)
-            .collect(Collectors.joining("\n"))),
-        COUNT_WORD("Write the count for each word", 
-            s -> Arrays.stream(s.split(" "))
-            .collect(Collectors.groupingBy(word -> word, Collectors.counting()))
-            .entrySet().stream()
-            .map(entry -> entry.getKey() + "->" + entry.getValue())
-            .collect(Collectors.joining("\n")));
+        ALPHABETICAL_ORDER(
+            "List all the words in alphabetical order", 
+            s -> Arrays.stream(s.split("\\s+"))
+                .sorted(String::compareToIgnoreCase)
+                .collect(Collectors.joining("\n"))
+        ),
+        COUNT_WORD(
+            "Write the count for each word", 
+            s -> Arrays.stream(s.split("\\s+"))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .map(entry -> entry.getKey() + "->" + entry.getValue())
+                .collect(Collectors.joining("\n"))
+        );
 
         private final String commandName;
         private final Function<String, String> fun;
